ac8ff5d6a1d2115579e6fc575eee5bdaac45eb04
prashanthi-anam <113832925+prashanthi-anam@users.noreply.github.com>
2023-11-29 10:31:26 +0530

    loganalytics.tf

--- demo1/loganalytics.tf
+++ demo1/loganalytics.tf
@@ -1,9 +1,21 @@
 
 resource "azurerm_log_analytics_workspace" "example" {
-  name                = "workspace-01"
+  name                = "workspace-01-${random_string.random.result}"
   location            = azurerm_resource_group.rg1.location
   resource_group_name = azurerm_resource_group.rg1.name
   sku                 = "PerGB2018"
   retention_in_days   = 30
+}
+resource "azurerm_log_analytics_solution" "example" {
+  solution_name         = "ContainerInsights"
+  location              = azurerm_resource_group.rg1.location
+  resource_group_name   = azurerm_resource_group.rg1.name
+  workspace_resource_id = azurerm_log_analytics_workspace.example.id
+  workspace_name        = azurerm_log_analytics_workspace.example.name
+
+  plan {
+    publisher = "Microsoft"
+    product   = "OMSGallery/ContainerInsights"
+  }
 }
 
