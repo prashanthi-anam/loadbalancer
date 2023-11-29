b28842c676892f7d8656bf015fdf5f9bf9259850
prashanthi-anam <113832925+prashanthi-anam@users.noreply.github.com>
2023-11-29 10:33:06 +0530

    vm-monitor.tf

--- demo1/vm-monitor.tf
+++ demo1/vm-monitor.tf
@@ -1,19 +1,64 @@
+
 resource "azurerm_monitor_data_collection_rule" "example" {
-  name                = "example-dcr"
-  location            = azurerm_resource_group.rg1.location
+
+
+  name                = "vminsights"
   resource_group_name = azurerm_resource_group.rg1.name
-  destinations {
-    azure_monitor_metrics {
-      name = "example-destination-metrics"
+  location            = azurerm_resource_group.rg1.location
+
+  data_flow {
+    destinations = ["log-analytics"]
+    streams = [
+      "Microsoft-Event",
+      "Microsoft-InsightsMetrics",
+      "Microsoft-Perf",
+      "Microsoft-ServiceMap"
+    ]
+  }
+
+  data_flow {
+    destinations = ["monitor-metrics"]
+    streams      = ["Microsoft-InsightsMetrics"]
+  }
+
+  data_sources {
+    extension {
+      extension_name = "DependencyAgent"
+      name           = "DependencyAgentDataSource"
+      streams        = ["Microsoft-ServiceMap"]
+    }
+
+    performance_counter {
+      counter_specifiers            = ["\\VmInsights\\DetailedMetrics"]
+      name                          = "insights-metrics"
+      sampling_frequency_in_seconds = 60
+      streams = [
+        "Microsoft-InsightsMetrics",
+        "Microsoft-Perf"
+      ]
+    }
+
+    windows_event_log {
+      name    = "windows-events"
+      streams = ["Microsoft-Event"]
+      x_path_queries = [
+        "Application!*[System[(Level=1 or Level=2 or Level=3)]]",
+        "System!*[System[(Level=1 or Level=2 or Level=3)]]"
+      ]
     }
   }
-  data_flow {
-    streams      = ["Microsoft-InsightsMetrics"]
-    destinations = ["example-destination-metrics"]
-  }
-  depends_on = [azurerm_resource_group.rg1, azurerm_lb.example, azurerm_windows_virtual_machine.example]
-}
 
+  destinations {
+    azure_monitor_metrics {
+      name = "monitor-metrics"
+    }
+
+    log_analytics {
+      name                  = "log-analytics"
+      workspace_resource_id = azurerm_log_analytics_workspace.example.id
+    }
+  }
+}
 resource "azurerm_monitor_data_collection_endpoint" "example" {
   name                = "example-dce"
   location            = azurerm_resource_group.rg1.location
@@ -32,13 +77,15 @@ resource "azurerm_monitor_data_collection_rule_association" "example1" {
 }
 
 # associate to a Data Collection Endpoint
+
 resource "azurerm_monitor_data_collection_rule_association" "example2" {
+  name                        = "configurationAccessEndpoint"
   target_resource_id          = azurerm_windows_virtual_machine.example.id
   data_collection_endpoint_id = azurerm_monitor_data_collection_endpoint.example.id
   description                 = "example"
-  depends_on                  = [azurerm_resource_group.rg1, azurerm_lb.example, azurerm_windows_virtual_machine.example, azurerm_monitor_data_collection_endpoint.example]
+  depends_on                  = [azurerm_resource_group.rg1, azurerm_lb.example, azurerm_windows_virtual_machine.example, azurerm_monitor_data_collection_endpoint.example, azurerm_monitor_data_collection_rule.example]
 }
-resource "azurerm_monitor_diagnostic_setting" "example" {
+/*resource "azurerm_monitor_diagnostic_setting" "example" {
   name               = "example-diagnostic-setting"
   target_resource_id = azurerm_windows_virtual_machine.example.id
 
@@ -48,4 +95,6 @@ resource "azurerm_monitor_diagnostic_setting" "example" {
     category = "AllMetrics"
     enabled  = true
   }
-}
+}*/
+
+
